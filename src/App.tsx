6587133b1b5b2fb306778ba0fd909c7178551a5a
Robel <robelalemu2372@gmail.com>
2023-10-05 21:09:25 +0300

    built main initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { Box, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Grid templateAreas={`"nav" "main" "footer"`}>
+      <GridItem area="nav" bg="green">
+        Nav
+      </GridItem>
+
+      <GridItem area="main" bg="yellow">
+        Main
+      </GridItem>
+      <GridItem area="footer" bg="red">
+        Footer
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
