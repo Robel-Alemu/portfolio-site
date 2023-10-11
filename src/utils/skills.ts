838141cd560fc96db1bb9c6f07897ceb476b5eee
Robel <robelalemu2372@gmail.com>
2023-10-11 13:38:51 +0300

    added skill assets

---
+++ src/utils/skills.ts
@@ -0,0 +1,21 @@
+import react from "../assets/react.svg";
+import node from "../assets/nodejs-removebg-preview.png";
+import javascript from "../assets/js.png";
+import typescript from "../assets/Typescript..png";
+import csharp from "../assets/c_-removebg-preview.png";
+import wordpress from "../assets/wordpress-removebg-preview.png";
+import mysql from "../assets/mysql-removebg-preview (1).png";
+import aws from "../assets/aws-removebg-preview.png";
+
+const skills = [
+  { name: "React", logo: react },
+  { name: "Node Js", logo: node },
+  { name: "JavaScript", logo: javascript },
+  { name: "TypeScript", logo: typescript },
+  { name: "C#", logo: csharp },
+  { name: "Wordpress", logo: wordpress },
+  { name: "MySQL", logo: mysql },
+  { name: "AWS", logo: aws },
+];
+
+export default skills;
