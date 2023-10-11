d1f543ca3b74ca96040c06ee10aadd070217675a
Robel <robelalemu2372@gmail.com>
2023-10-11 23:31:48 +0300

    added more skills

--- src/utils/skills.ts
+++ src/utils/skills.ts
@@ -6,16 +6,24 @@ import csharp from "../assets/csharp.png";
 import wordpress from "../assets/wp.png";
 import mysql from "../assets/mysqll.png";
 import aws from "../assets/awss.png";
+import git from "../assets/git.png";
+import firebase from "../assets/firebase.png";
+import chakra from "../assets/chakra.png";
+import bootstrap from "../assets/Bootstrap2.png";
 
 const skills = [
   { name: "React", logo: react, display: "flex" },
   { name: "Node Js", logo: node, display: "flex" },
   { name: "JavaScript", logo: javascript, display: "flex" },
   { name: "TypeScript", logo: typescript, display: "flex" },
-  { name: "C#", logo: csharp, display: "none" },
-  { name: "Wordpress", logo: wordpress, display: "none" },
-  { name: "MySQL", logo: mysql, display: "none" },
-  { name: "AWS", logo: aws, display: "none" },
+  { name: "C#", logo: csharp, display: "flex" },
+  { name: "Wordpress", logo: wordpress, display: "flex" },
+  { name: "MySQL", logo: mysql, display: "flex" },
+  { name: "AWS", logo: aws, display: "flex" },
+  { name: "Firebase", logo: firebase, display: "none" },
+  { name: "Git", logo: git, display: "none" },
+  { name: "Bootstrap", logo: bootstrap, display: "none" },
+  { name: "Chakra UI", logo: chakra, display: "none" },
 ];
 
 export default skills;
