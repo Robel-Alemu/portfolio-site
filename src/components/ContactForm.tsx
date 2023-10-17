27d453b6dc1f194c15d52aba2b258d66d3e2d03d
Robel <robelalemu2372@gmail.com>
2023-10-18 01:18:09 +0300

    added api to send email

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -7,17 +7,50 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import styles from "../styles/Contact.module.css";
+import { useRef } from "react";
+
 const ContactForm = () => {
+  const nameRef = useRef<HTMLInputElement | null>(null);
+  const emailRef = useRef<HTMLInputElement | null>(null);
+  const subjectRef = useRef<HTMLInputElement | null>(null);
+  const messageRef = useRef<HTMLTextAreaElement | null>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const name = nameRef.current?.value || "";
+    const email = emailRef.current?.value || "";
+    const subject = subjectRef.current?.value || "";
+    const message = messageRef.current?.value || "";
+    const inbox = {
+      name: name,
+      email: email,
+      subject: subject,
+      message: message,
+    };
+    console.log(inbox);
+    fetch("http://localhost:8080/api/send-mail", {
+      method: "POST",
+      body: JSON.stringify(inbox),
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => {
+      console.log(response.status);
+    });
+  };
+
   return (
     <Box>
       <Text fontSize="30px" paddingBottom={5}>
         Get in touch
       </Text>
+
       <FormControl isRequired>
         <Input
           className={styles.input}
           placeholder="Your Name"
           marginBottom="35px"
+          type="text"
+          ref={nameRef}
           paddingY={6}
         />
 
@@ -26,25 +59,30 @@ const ContactForm = () => {
           type="email"
           placeholder="Your Email"
           marginBottom="35px"
+          ref={emailRef}
           paddingY={6}
         />
         <Input
           className={styles.input}
           placeholder="Subject"
           marginBottom="35px"
+          ref={subjectRef}
+          type="text"
           paddingY={6}
         />
         <Textarea
           className={styles.input}
           placeholder="Your Comment..."
           marginBottom="10px"
+          ref={messageRef}
           rows={6}
         />
         <Button
           marginTop="35px"
           className={styles.contactButton}
           marginBottom="35px"
-          type="submit"
+          // type="submit"
+          onClick={submitHandler}
         >
           Send Message
         </Button>
